087ba1a3674844c51cc4ac98e441b26d1284d620
Ajay <ajaysingh01103@gmail.com>
2026-01-17 15:52:09 +0530

    open api

--- apps/api/main.ts
+++ apps/api/main.ts
@@ -1,14 +1,16 @@
 import { Elysia } from "elysia";
 import { node } from "@elysiajs/node";
+import { openapi } from "@elysiajs/openapi";
 
 import { cors } from "@elysiajs/cors";
 
 const app = new Elysia({ adapter: node() })
   .use(cors())
+  .use(openapi())
   .get("/", () => ({ hello: "Node.js!!!!!!!👋" }))
   .get("/hello", () => ({ hello: "Hello World" }))
   .listen(3000);
 
 console.log(`Listening on http://localhost:3000`);
 
-export type AppType = typeof app 
+export type AppType = typeof app;
